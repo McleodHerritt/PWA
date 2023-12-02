429f2c18e3df8a6b4aeb1dc2564f85bcdb865fc4
McleodHerritt <nmcleod2021@gmail.com>
2023-12-02 18:19:39 -0400

    fixed file name error for the service worker plugin

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -2,9 +2,38 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+const workboxPlugin = new InjectManifest({
+  swSrc: "./src-sw.js",
+  swDest: "src-sw.js",
+});
+
+const manifestPlugin = new WebpackPwaManifest({
+  name: "My Progressive Web App",
+  short_name: "My PWA",
+  description: "This is my Progressive Web App",
+  background_color: "#ffffff",
+  theme_color: "#000000",
+});
+
 // TODO: Add CSS loaders and babel to webpack.
+const cssLoader = {
+  test: /\.css$/i,
+  use: [MiniCssExtractPlugin.loader, "css-loader"],
+};
+
+const babelLoader = {
+  test: /\.m?js$/,
+  exclude: /(node_modules|bower_components)/,
+  use: {
+    loader: "babel-loader",
+    options: {
+      presets: ["@babel/preset-env"],
+    },
+  },
+};
 
 module.exports = () => {
   return {
@@ -23,22 +52,12 @@ module.exports = () => {
         title: "Webpack Plugin",
       }),
       new MiniCssExtractPlugin(),
+      workboxPlugin,
+      manifestPlugin,
     ],
 
     module: {
-      rules: [
-        { test: /\.css$/i, use: [MiniCssExtractPlugin.loader, "css-loader"] },
-        {
-          test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env"],
-            },
-          },
-        },
-      ],
+      rules: [cssLoader, babelLoader],
     },
   };
 };
